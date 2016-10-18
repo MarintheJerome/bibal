021adc8a4c1fa192ee79686f216afd05dcf91ee9
Yhugo <yohann.hugo@gmail.com>
2016-10-18 10:00:02 +0200

    View V0.1 et Structure

---
+++ src/main/java/controller/MainViewController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Yhugo on 18/10/2016.
+ */
+public class MainViewController implements Initializable {
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
