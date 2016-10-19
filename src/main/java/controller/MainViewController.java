0f007514308c5518168b1bf36ee245258ab3371b
Yhugo <yohann.hugo@gmail.com>
2016-10-19 11:42:38 +0200

    perfectionnement vue

--- src/main/java/controller/MainViewController.java
+++ src/main/java/controller/MainViewController.java
@@ -1,7 +1,16 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -10,7 +19,36 @@ import java.util.ResourceBundle;
  */
 public class MainViewController implements Initializable {
 
-    public void initialize(URL location, ResourceBundle resources) {
+    @FXML
+    public MenuItem about;
 
+    @FXML
+    public AnchorPane panelView;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        EventHandler actionMenu = afficheMenu();
+        about.setOnAction(actionMenu);
     }
+
+    private EventHandler<ActionEvent> afficheMenu() {
+        return new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                MenuItem mItem = (MenuItem) event.getSource();
+                String id = mItem.getId();
+                if ("about".equalsIgnoreCase(id)) {
+                    panelView.getChildren().clear();
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("About.fxml"));
+                    Parent root = null;
+                    try {
+                        root = fxmlLoader.load();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    panelView.getChildren().add(root);
+                }
+            }
+        };
+    }
+
 }
